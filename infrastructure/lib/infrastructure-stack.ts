434ff6da8954ac874990eec4e3dd98b2cf9cb83c
Zhabskyi <poltavagaz@gmail.com>
2024-04-13 21:11:21 -0600

    circleci file

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -6,6 +6,9 @@ import * as cloudFront from "aws-cdk-lib/aws-cloudfront";
 import { Construct } from "constructs";
 
 import appConfig from "./app.config.json";
+import { BlockPublicAccess, BucketEncryption } from "aws-cdk-lib/aws-s3";
+import { RemovalPolicy, Stack } from "aws-cdk-lib";
+import { CfnRole, Effect, PolicyStatement, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 
 const appName = `${appConfig.appName}`;
 
@@ -16,11 +19,29 @@ export class InfrastructureStack extends cdk.Stack {
     // Add S3 Bucket
     const s3Site = new s3.Bucket(this, `${appConfig.context}-${appConfig.appName}-bucket`, {
       bucketName: `${appConfig.context}-${appConfig.appName}-s3-bucket`,
-      publicReadAccess: true,
+      publicReadAccess: false,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      encryption: BucketEncryption.S3_MANAGED,
       websiteIndexDocument: "index.html",
       websiteErrorDocument: "index.html",
     });
-    this.enableCorsOnBucket(s3Site);
+
+    s3Site.addToResourcePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["s3:GetObject"],
+        principals: [new ServicePrincipal("cloudfront.amazonaws.com")],
+        resources: [s3Site.bucketArn + "/*"],
+        conditions: {
+          StringLike: {
+            //Only allow cloudfront distributions of the same account to read from bucket
+            "AWS:SourceARN": [`arn:aws:cloudfront::${Stack.of(this).account}:distribution/*`],
+          },
+        },
+      }),
+    );
 
     // Create a new CloudFront Distribution
     const distribution = new cloudFront.CloudFrontWebDistribution(
@@ -58,6 +79,7 @@ export class InfrastructureStack extends cdk.Stack {
     new s3deploy.BucketDeployment(this, `${appConfig.context}-${appConfig.appName}--s3bucket-deployment`, {
       sources: [s3deploy.Source.asset("../build")],
       destinationBucket: s3Site,
+      destinationKeyPrefix: appConfig.appName,
       distribution: distribution,
       distributionPaths: ["/*"],
     });
@@ -67,18 +89,4 @@ export class InfrastructureStack extends cdk.Stack {
       value: distribution.distributionDomainName,
     });
   }
-
-  enableCorsOnBucket = (bucket: s3.IBucket) => {
-    const cfnBucket = bucket.node.findChild("Resource") as s3.CfnBucket;
-    cfnBucket.addPropertyOverride("CorsConfiguration", {
-      CorsRules: [
-        {
-          AllowedOrigins: ["*"],
-          AllowedMethods: ["HEAD", "GET", "PUT", "POST", "DELETE"],
-          ExposedHeaders: ["x-amz-server-side-encryption", "x-amz-request-id", "x-amz-id-2"],
-          AllowedHeaders: ["*"],
-        },
-      ],
-    });
-  };
 }
